1b5ea50adb23a95e3d0120ef4046a4da4de414e1
TechDomani <73493660+TechDomani@users.noreply.github.com>
2024-11-28 13:54:04 +0000

    Initial commit

---
+++ ExceptionExercises/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ExceptionExercises
+{
+    internal class Program
+    {
+        static void Main()
+        {            
+            CakeDivision();
+            //DetermineHouse();
+            //ReadDiary();
+        }
+               
+        public static void CakeDivision()
+        {
+            int cakeSlices = 32;
+            Console.WriteLine("Please enter the number of children");
+            int children = Convert.ToInt32(Console.ReadLine());
+            int slices = cakeSlices / children;
+            Console.WriteLine($"Each child will get {slices} slices");
+        }
+
+        public static void ReadDiary()
+        {
+            string fileName = @"Files\DiaryEvents.csv";
+            List<DiaryRecord> diaryRecords = Diary.ReadDiaryEvents(fileName);
+            foreach (var entry in diaryRecords)
+            {
+                Console.WriteLine(entry);
+            }
+        }
+
+        public static void DetermineHouse()
+        {
+            Console.WriteLine("Which house do you want to be added to?");
+            string userHouse = Console.ReadLine() ?? string.Empty;
+            House house = SortingHat.FindHouse(userHouse);
+            Console.WriteLine($"You have been assigned to {house}");
+        }
+
+
+}
+}
